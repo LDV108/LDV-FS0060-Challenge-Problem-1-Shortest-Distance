4d6d062b995d4cee1e245f8e56dd50274f12e5ba
Luigi Del Valle <delvalle.luigi@gmail.com>
2023-10-09 02:40:01 +0100

    V1

---
+++ C1/ShortestDistance.py
@@ -0,0 +1,34 @@
+import math
+
+# 52.6369° N, 1.1398° W - LE
+# 37.7749° N, 122.4194° W - SF
+# Earth radius = 6.371 km
+
+EARTH_RADIUS = 6371
+DEG_TO_KM = 111.32
+LE_LONG = 52.6369
+LE_LAT = 1.1398
+SF_LONG = 37.7749
+SF_LAT = 122.4194
+
+
+def gdc_calc(lo1, la1, lo2, la2):
+    latDiff = abs(la2 - la1)
+    longDiff = abs(lo1 - lo2)
+    cntrl_ang = math.acos((math.sin(la1) * math.sin(la2)) + (math.cos(la1) * math.cos(la2) * math.cos(longDiff)))
+    print(cntrl_ang)
+    arcLen = EARTH_RADIUS * cntrl_ang
+
+    return arcLen
+
+
+def degtokm_calc(dis):
+    km = dis * DEG_TO_KM
+    return km
+
+
+distanceDeg = gdc_calc(LE_LONG, LE_LAT, SF_LONG, SF_LAT)
+print(distanceDeg)
+
+distanceKM = degtokm_calc(distanceDeg)
+print(distanceKM)
