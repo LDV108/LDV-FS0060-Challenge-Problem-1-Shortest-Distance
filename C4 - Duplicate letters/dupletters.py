9ba0a8a77943d165e5e24b1bfafcac7728d6b0ec
Luigi Del Valle <delvalle.luigi@gmail.com>
2024-03-12 01:55:54 +0000

    V1

---
+++ C4 - Duplicate letters/dupletters.py
@@ -0,0 +1,29 @@
+
+
+def checkDuplicate(sen):
+    sentence = sen
+    sentenceSP = sentence.lower().split()
+    print(sentenceSP)
+    sentenceSP2 = []
+    letterList = []
+    flag = False
+
+    for i in sentenceSP:
+        for j in i:
+            sentenceSP2.append(j)
+
+    print(sentenceSP2)
+    for m in range(len(sentenceSP2)):
+        if sentenceSP2[m] not in letterList:
+            letterList.append(sentenceSP2[m])
+            # print(sentenceSP2[m],letterList)
+        else:
+            flag = True
+
+    print(letterList)
+    return flag
+sentenceINP = str(input("Input sentence: "))
+print(checkDuplicate(sentenceINP))
+
+
+
